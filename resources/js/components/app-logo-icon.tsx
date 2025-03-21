6b8923ff85c514c85bec906bee3bcd264b9eeb9f
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-03-21 13:16:26 -0700

    fix: icon color

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -8,7 +8,6 @@ export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="none"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
